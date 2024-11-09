9092d823d26d35aefedf4d1bbb0983507f089894
plotva <huseva.a@gmail.com>
2024-11-09 14:19:05 +0100

    Update google login and signup dto file names

--- src/modules/auth/dto/google-login-dto.ts
+++
@@ -1,9 +0,0 @@
-export class GoogleLoginDto {
-    id: string;
-    email: string;
-    firstName: string;
-    lastName: string;
-    avatar: string;
-    accessToken: string;
-    refreshToken: string;
-}
